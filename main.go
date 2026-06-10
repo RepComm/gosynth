e7f258d8ff9b12ead12789adbb84f9b06c757a70
Addi <rccommandotac@gmail.com>
2026-06-10 16:47:44 -0500

    multiple oscillators working!

--- main.go
+++ main.go
@@ -35,22 +35,23 @@ func main() {
 	otoPlayer.Play()
 
 	//start our synth engine
-	go se.Synth()
+	go se.Worker()
 
 	HandleSigTerm(func(sig os.Signal) {
 		os.Exit(0)
 	})
 
 	for {
-		time.Sleep(10 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 }
 
 type SynthEngine struct {
-	SampleRate    float32
-	GlobalTime    float32
+	SampleRate    float64
+	GlobalTime    float64
 	OutputChannel *RingBuffer
 	OutputReader  *RingReader
+	Oscs          []*Osc
 }
 
 func NewSynthEngine() (*SynthEngine, error) {
@@ -63,46 +64,109 @@ func NewSynthEngine() (*SynthEngine, error) {
 		GlobalTime:    0,
 		OutputChannel: rb,
 		OutputReader:  &RingReader{RB: rb},
+		Oscs: []*Osc{
+			&Osc{
+				Type:      OSC_TYPE_SIN,
+				Frequency: 440.0,
+				Amplitude: 0.5,
+			},
+			&Osc{
+				Type:      OSC_TYPE_SAW,
+				Frequency: 440.0,
+				Amplitude: 0.1,
+			},
+		},
 	}, nil
 }
 
-const SYNTH_BLOCK_SIZE = 512
+const SYNTH_BLOCK_SIZE = 1024
+const SYNTH_BLOCK_LOW = 256
+const SYNTH_WORKER_SLEEP_MS = 5
+
+type OscType int
+
+const (
+	OSC_TYPE_SIN OscType = iota
+	OSC_TYPE_COS
+	OSC_TYPE_SAW
+	OSC_TYPE_SQUARE
+	OSC_TYPE_TRIANGLE
+)
+
+type Osc struct {
+	Frequency float64
+	Amplitude float64
+	Phase     float64
+	Type      OscType
+}
+
+const Tau float64 = math.Pi * 2
+
+func (osc *Osc) Sample64(sampleRate float64) float64 {
+
+	var sample float64
+
+	switch osc.Type {
+	case OSC_TYPE_SIN:
+		sample = math.Sin(osc.Phase)
+
+	case OSC_TYPE_SAW:
+		sample = osc.Phase/math.Pi - 1
+
+	case OSC_TYPE_SQUARE:
+		if osc.Phase < math.Pi {
+			sample = 1
+		} else {
+			sample = -1
+		}
+	}
+
+	osc.Phase += Tau * osc.Frequency / sampleRate
+	if osc.Phase >= Tau {
+		osc.Phase -= Tau
+	}
+
+	return sample * osc.Amplitude
+}
 
 func (se *SynthEngine) Generate(block []float32) int {
 
 	phase := se.GlobalTime
-	var sinFreq float32 = 440.0
-	var sinAmp float32 = 0.5
-
-	phaseStep := float32(2*math.Pi) * sinFreq / se.SampleRate
 
 	blockSize := len(block)
+	var sample64 float64
 	for i := 0; i < blockSize; i++ {
-		block[i] = Sin32(phase) * sinAmp
-		phase += phaseStep
+		sample64 = 0
+
+		for _, osc := range se.Oscs {
+			sample64 += osc.Sample64(se.SampleRate)
+		}
+
+		block[i] = float32(sample64)
 	}
 
 	se.GlobalTime = phase
 	return blockSize
 }
 
-func (se *SynthEngine) Synth() {
+func (se *SynthEngine) Worker() {
 
 	var block [SYNTH_BLOCK_SIZE]float32
 
-	i := 0
 	for {
-		se.Generate(block[:])
-		se.OutputChannel.Write(block[:])
-		i++
-		// if n < 1 {
-		// 	break
-		// }
+		available := se.OutputChannel.Available()
+		// fmt.Printf(
+		// 	"avail=%d free=%d write=%d read=%d\n",
+		// 	se.OutputChannel.Available(),
+		// 	se.OutputChannel.Free(),
+		// 	se.OutputChannel.WritePos.Load(),
+		// 	se.OutputChannel.ReadPos.Load(),
+		// )
+		if available < SYNTH_BLOCK_LOW {
+			se.Generate(block[:])
+			se.OutputChannel.Write(block[:])
+		} else {
+			time.Sleep(SYNTH_WORKER_SLEEP_MS * time.Millisecond)
+		}
 	}
-	// fmt.Println("wrote", i)
-
-}
-
-func Sin32(f float32) float32 {
-	return float32(math.Sin(float64(f)))
 }
